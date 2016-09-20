4ddce7a6a47fd354c3eec1a56321c392e32c335c
liujinliu <liujinliu@le.com>
2016-09-20 16:10:58 +0800

    add auto check scripts

--- scripts/docker/init/init.sh
+++ scripts/docker/init/init.sh
@@ -44,3 +44,5 @@ cd /usr/local/LeMonitor/falcon-agent
 sed -i "s/127\.0\.0\.1/${IP}/g" cfg.json
 sed -i "s/ihbs\.sys\.monitor\.letv\.cn/10\.183\.96\.57/g" cfg.json
 cd -
+chmod +x /opt/letv/falcon-agent/check_scripts/check_all.sh
+echo "*/3 * * * * root /opt/letv/falcon-agent/check_scripts/check_all.sh >>/dev/null">> /etc/crontab
---
+++ scripts/rpm/build/opt/letv/falcon-agent/check_scripts/check_all.sh
@@ -0,0 +1,2 @@
+timeout 1m curl "http://127.0.0.1:8000/alarms/query/52/mysql"
+
