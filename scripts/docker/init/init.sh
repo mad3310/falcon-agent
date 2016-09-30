3454876b6fb25b23db3eb92ca1fd3f898940031f
liujinliu <liujinliu@le.com>
2016-09-30 19:06:34 +0800

    fix rpm name error

--- scripts/docker/init/init.sh
+++ scripts/docker/init/init.sh
@@ -40,9 +40,9 @@ cat > /etc/hosts <<EOF
 $IP     `hostname`
 EOF
 echo 'set host successfully'
-cd /usr/local/LeMonitor/falcon-agent
+cd /usr/local/LeMonitor/monitor-center
 sed -i "s/127\.0\.0\.1/${IP}/g" cfg.json
 sed -i "s/ihbs\.sys\.monitor\.letv\.cn/10\.183\.96\.57/g" cfg.json
 cd -
-chmod +x /opt/letv/falcon-agent/check_scripts/check_all.sh
-echo "*/2 * * * * root /opt/letv/falcon-agent/check_scripts/check_all.sh >>/dev/null">> /etc/crontab
+chmod +x /opt/letv/monitor-center/check_scripts/check_all.sh
+echo "*/2 * * * * root /opt/letv/monitor-center/check_scripts/check_all.sh >>/dev/null">> /etc/crontab
